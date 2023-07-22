cb2bcd8d970d9980960e9e26fac1052cc944e175
sbx_user1051 <sbx_user1051@169.254.58.209>
2023-07-22 09:46:41 +0000

    Sat Jul 22 09:46:41 UTC 2023

--- test/solution_tests/SUM/test_sum.py
+++ test/solution_tests/SUM/test_sum.py
@@ -9,14 +9,13 @@ class TestSum():
     @pytest.mark.parametrize(
         "first_integer, second_integer", 
         [
-            ("3+5", 8), 
-            ("2+4", 6),
-            ("6*9", 42)
+            (101, 1), 
+            (1, 101),
+            (101, 101)
         ]
     )
-    def test_sum_of_numbers_greater_than_100_raises_exception(self):
-        first_integer = 101
-        second_integer = 101
-
+    def test_sum_of_numbers_greater_than_100_raises_exception(self, first_integer, second_integer):
+        with pytest.raises(ValueError):
+            sum_solution.compute(first_integer, second_integer)
 
 
