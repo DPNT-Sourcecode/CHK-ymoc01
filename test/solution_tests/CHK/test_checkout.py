89c183539d3f499280f603a1aa615808f981dc35
sbx_user1051 <sbx_user1051@169.254.58.209>
2023-07-22 12:07:39 +0000

    Sat Jul 22 12:07:39 UTC 2023

--- test/solution_tests/CHK/test_checkout.py
+++ test/solution_tests/CHK/test_checkout.py
@@ -1,5 +1,6 @@
+from unittest.mock import patch
 from solutions.CHK import checkout_solution
-from solutions.CHK.models import Offer
+from solutions.CHK.models import Offer, load_offers
 
 # TODO what happens if the prices change, which reqs say offers do weekly
 # fixtures and patching probably quickest way around this, prefer factoryboy
@@ -35,9 +36,18 @@ def test_offer_apply_no_offer_returns_zero_price_and_unchanged_count():
     offer = Offer(quantity=999, price=999)
     assert offer.apply(1) == (0, 1)
 
+@patch(
+    "solutions.CHK.static_prices.OFFERS", {   
+        "A": {
+            "quantity": 1,
+            "price": 10,
+        },
+    }
+)
 def test_load_offers_correctly_loads_offer_as_object():
-    ...
-    
+    loaded_offers = load_offers()
+    assert len(loaded_offers, 1)
+
 def test_calculate_price_of_offers_for_multiple_offers_calculates_lowest_price_combination():
     ...
     
