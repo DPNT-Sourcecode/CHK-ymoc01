b2f58a26fe747efea586202f172dbb9ececc9b42
sbx_user1051 <sbx_user1051@169.254.58.209>
2023-07-22 10:10:50 +0000

    Sat Jul 22 10:10:50 UTC 2023

--- test/solution_tests/SUM/test_sum.py
+++ test/solution_tests/SUM/test_sum.py
@@ -3,7 +3,14 @@ from solutions.SUM import sum_solution
 
 
 class TestSum():
-    def test_sum_of_valid_numbers_returns_sum(self):
+    @pytest.mark.parametrize(
+        "first_integer, second_integer", 
+        [
+            (0, 1), 
+            (1, 2),
+        ]
+    )
+    def test_sum_of_valid_numbers_returns_sum(self, first_integer, second_integer):
         assert sum_solution.compute(1, 2) == 3
 
     @pytest.mark.parametrize(
