ed5a45b56844f74413b992543a88a34a9eb73314
sbx_user1051 <sbx_user1051@169.254.58.209>
2023-07-22 14:34:39 +0000

    Sat Jul 22 14:34:39 UTC 2023

--- test/solution_tests/CHK/test_checkout.py
+++ test/solution_tests/CHK/test_checkout.py
@@ -1,3 +1,4 @@
+from requests import patch
 from solutions.CHK import checkout_solution
 from solutions.CHK.models import FreeProductSideEffect, Offer
 
@@ -30,6 +31,71 @@ def test_checkout_for_basket_with_free_product_which_has_offer_removes_offer():
 def test_checkout_for_basket_with_lots_of_products_with_overlapping_rules():
     sku_string = "AAAAABBBBCCDDDDEEEE"
     assert checkout_solution.checkout(sku_string) == 200 + 45 + 40 + 60 + 160
+
+@patch(
+    "solutions.CHK.models.load_offers"
+)
+def test_calculate_price_of_offers_for_single_offer_returns_offer_price_and_adjusted_count(load_offers_mock):
+    offer = Offer(quantity=3, price=999)
+    
+    offers = [offer]
+    count = 3
+    
+    total_offer_price, count_after_offers, _ = checkout_solution.calculate_price_of_offers(offers, count)
+    
+    assert total_offer_price == 999
+    assert count_after_offers == 0
+
+# def test_calculate_price_of_offers_for_no_offer_returns_zero_price_and_unchanged_count():
+#     offer = Offer(quantity=999, price=999)
+
+#     offers = [offer]
+#     count = 1
+
+#     total_offer_price, count_after_offers, _ = checkout_solution.calculate_price_of_offers(offers, count)
+    
+#     assert total_offer_price == 0
+#     assert count_after_offers == 1
+
+# def test_calculate_price_of_offers_for_multiple_offers_prioritises_high_quantity():
+#     offer_for_three = Offer(quantity=3, price=130)
+#     offer_for_five = Offer(quantity=5, price=200)
+
+#     offers = [offer_for_three, offer_for_five]
+#     count = 6
+
+#     total_offer_price, count_after_offers, _ = checkout_solution.calculate_price_of_offers(offers, count)
+
+#     assert total_offer_price == 200
+#     assert count_after_offers == 1
+
+# def test_calculate_price_of_offers_for_multiple_offers_calculates_applies_both_offers():
+#     offer_for_three = Offer(quantity=3, price=130)
+#     offer_for_five = Offer(quantity=5, price=200)
+
+#     offers = [offer_for_three, offer_for_five]
+#     count = 9
+
+#     total_offer_price, count_after_offers, _ = checkout_solution.calculate_price_of_offers(offers, count)
+
+#     assert total_offer_price == 330
+#     assert count_after_offers == 1
+
+# def test_calculate_price_of_offers_with_side_effect_returns_products_for_removal():
+#     offer_with_side_effect = Offer(quantity=2, price=80, side_effect=FreeProductSideEffect(product="B"))
+
+#     offers = [offer_with_side_effect]
+#     count = 2
+
+#     total_offer_price, count_after_offers, products_for_removal = checkout_solution.calculate_price_of_offers(offers, count)
+
+#     assert total_offer_price == 80
+#     assert count_after_offers == 0
+#     assert products_for_removal == ["B"]
+
+
+
+
 
 
 
