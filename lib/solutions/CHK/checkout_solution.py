f6abe4c2e263af4a51df3cce9307438d5d67a6a3
sbx_user1051 <sbx_user1051@169.254.58.209>
2023-07-22 11:00:17 +0000

    Sat Jul 22 11:00:17 UTC 2023

--- lib/solutions/CHK/checkout_solution.py
+++ lib/solutions/CHK/checkout_solution.py
@@ -10,20 +10,28 @@ def checkout(skus: str) -> int:
     offers = OFFERS
 
     # For now, assuming SKUs are structured like AAABCAD
-    product_counts: defaultdict(int) = {}
+    product_counts = defaultdict(int)
     for sku in skus:
         product_counts[sku] += 1
  
     basket_total = 0
     for product, count in product_counts.items():
-        offer = offers.get("product")
+        offer = offers.get(product)
         if offer:
+            basket_total = calculate_offer_value(offer, count)
+            adjusted_count = count - offer["quantity"]
+        else:
+            adjusted_count = count
+        
+        
+
+        
+
             
 
 
 
     return basket_total
 
-
-
-
+def calculate_offer_value(offer: dict[str, int], count: int) -> int:
+    ...
--- test/solution_tests/CHK/test_checkout.py
+++ test/solution_tests/CHK/test_checkout.py
@@ -7,4 +7,3 @@ def test_checkout_for_basket_with_item_and_no_offers_returns_item_price():
 def test_checkout_for_basket_with_items_valid_for_offers_returns_offer_price():
     sku_string = "AAA"
     assert checkout_solution.checkout(sku_string) == 130
-
