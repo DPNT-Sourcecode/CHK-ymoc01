2e1abeff806a81b242d0fa9bd47d8891e8575e42
sbx_user1051 <sbx_user1051@169.254.58.209>
2023-07-22 10:51:17 +0000

    Sat Jul 22 10:51:17 UTC 2023

--- lib/solutions/CHK/checkout_solution.py
+++ lib/solutions/CHK/checkout_solution.py
@@ -1,6 +1,5 @@
 
 
-from typing import List
 from solutions.CHK.static_prices import ITEM_PRICES, OFFERS
 
 # noinspection PyUnusedLocal
@@ -10,8 +9,14 @@ def checkout(skus: str) -> int:
     offers = OFFERS
 
     # For now, assuming SKUs are structured like AAABCAD
-    sum = 0
+    basket_total = 0
     for sku in skus:
+        try:
+            basket_total += prices[sku]
+        except KeyError:
+            return -1
+        
+    return basket_total
 
 
 
--- test/solution_tests/CHK/test_checkout.py
+++ test/solution_tests/CHK/test_checkout.py
@@ -2,5 +2,9 @@ from solutions.CHK import checkout_solution
 
 def test_checkout_for_basket_with_item_and_no_offers_returns_item_price():
     sku_string = "A"
-    assert checkout_solution(sku_string) == 50
+    assert checkout_solution.checkout(sku_string) == 50
+
+def test_checkout_for_basket_with_items_valid_for_offers_returns_offer_price():
+    sku_string = "AAA"
+    assert checkout_solution.checkout(sku_string) == 130
 
