e68f55401b57fa1382c9a40e8fe97506b776e313
sbx_user1051 <sbx_user1051@169.254.58.209>
2023-07-22 10:45:17 +0000

    Sat Jul 22 10:45:17 UTC 2023

--- lib/solutions/CHK/checkout_solution.py
+++ lib/solutions/CHK/checkout_solution.py
@@ -1,6 +1,11 @@
 
 
+from solutions.CHK.static_prices import ITEM_PRICES, OFFERS
+
 # noinspection PyUnusedLocal
 # skus = unicode string
-def checkout(skus):
-    raise NotImplementedError()
+def checkout(skus: str) -> int:
+    prices = ITEM_PRICES
+    offers = OFFERS
+
+
