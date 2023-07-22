3ed945605f11a90ae38df383e202cea4c8a59aaa
sbx_user1051 <sbx_user1051@169.254.58.209>
2023-07-22 09:49:41 +0000

    Sat Jul 22 09:49:41 UTC 2023

--- lib/solutions/SUM/sum_solution.py
+++ lib/solutions/SUM/sum_solution.py
@@ -1,5 +1,8 @@
 # noinspection PyShadowingBuiltins,PyUnusedLocal
 def compute(x: int, y: int) -> int:
+    if any([input_param >= 100 for input_param in [x, y]]):
+        raise ValueError("Inputs must be less 100")
+
     return x + y
 
 
--- test/solution_tests/SUM/test_sum.py
+++ test/solution_tests/SUM/test_sum.py
@@ -9,9 +9,21 @@ class TestSum():
     @pytest.mark.parametrize(
         "first_integer, second_integer", 
         [
-            (101, 1), 
-            (1, 101),
-            (101, 101)
+            (100, 1), 
+            (1, 100),
+            (100, 100)
+        ]
+    )
+    def test_sum_of_numbers_greater_than_100_raises_exception(self, first_integer, second_integer):
+        with pytest.raises(ValueError):
+            sum_solution.compute(first_integer, second_integer)
+
+    @pytest.mark.parametrize(
+        "first_integer, second_integer", 
+        [
+            (0, 99), 
+            (99, 0),
+            (0, 0)
         ]
     )
     def test_sum_of_numbers_greater_than_100_raises_exception(self, first_integer, second_integer):
