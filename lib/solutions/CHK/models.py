1bd896955f6b754efd609e2be66f9cabd8e0533d
sbx_user1051 <sbx_user1051@169.254.58.209>
2023-07-22 12:01:39 +0000

    Sat Jul 22 12:01:39 UTC 2023

--- lib/solutions/CHK/models.py
+++ lib/solutions/CHK/models.py
@@ -3,6 +3,8 @@
 from typing import Optional
 from pydantic import BaseModel
 
+from solutions.CHK.static_prices import OFFERS
+
 class FreeProductSideEffect(BaseModel):
     product: str
 
@@ -18,3 +20,13 @@ class Offer(BaseModel):
         count_after_offers = count - (self.quantity * int(count / self.quantity))
 
         return offers_price, count_after_offers
+    
+def load_offers() -> dict[str, Offer]:
+    offers = OFFERS
+    parsed_offers = []
+
+    for product, offer in offers.items():
+        parsed_offers[product] = Offer(
+            quantity=offer["quantity"], 
+            price=offer["price"]
+        )
